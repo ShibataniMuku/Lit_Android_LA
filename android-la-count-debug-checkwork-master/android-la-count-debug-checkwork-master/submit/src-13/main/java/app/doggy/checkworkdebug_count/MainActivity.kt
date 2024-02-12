7f62e8a6357c4ac1681af21b78cf60456c5cb0b8
shibatani muku <s276111@wakayama-u.ac.jp>
2024-02-12 12:54:55 +0900

    課題14

--- android-la-count-debug-checkwork-master/android-la-count-debug-checkwork-master/submit/src-13/main/java/app/doggy/checkworkdebug_count/MainActivity.kt
+++ android-la-count-debug-checkwork-master/android-la-count-debug-checkwork-master/submit/src-13/main/java/app/doggy/checkworkdebug_count/MainActivity.kt
@@ -5,9 +5,8 @@ import android.os.Bundle
 import app.doggy.checkworkdebug_count.databinding.ActivityMainBinding
 
 /**
- * 問題14
- * 3の倍数で"Fizz"、5の倍数で"Buzz"、15の倍数で"FizzBuzz"、それ以外では数字を表示してください。
- * ログ出力を使ってデバッグしてみましょう！
+ * 問題13
+ * activity_main.xmlのレイアウトを整えてください。
  */
 
 class MainActivity : AppCompatActivity() {
@@ -23,16 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.plusButton.setOnClickListener {
             count += 1
-
-            if (count % 3 == 0) {
-                binding.countText.text = "Fizz"
-            } else if (count % 5 == 0) {
-                binding.countText.text = "Buzz"
-            } else if (count % 15 == 0) {
-                binding.countText.text = "FizzBuzz"
-            } else {
-                binding.countText.text = count.toString()
-            }
+            binding.countText.text = count.toString()
         }
     }
 }
